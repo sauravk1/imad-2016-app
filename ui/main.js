fe7f09095d78880c64248e51a8cc25cccc969e5f
sauravk1 <ksaurav325@gmail.com>
2016-10-08 12:52:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var counter=0;
 button.onclick=function()
 {
     counter=counter+1;
-    var span=document.GetElementById('counter');
+    var span=document.GetElementById('count');
     span.innerHTML=counter.toString();
     
 };
