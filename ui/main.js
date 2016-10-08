ea5f55aef18d18acc88fadfca973d107a06340e7
sauravk1 <ksaurav325@gmail.com>
2016-10-08 12:47:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
-console.log('Loaded!');
-
-console.log('welcome');
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function()
+{
+    counter=counter+1;
+    var span=document.GetElementById('count');
+    span.innerHTML=counter.toString();
+    
+};
